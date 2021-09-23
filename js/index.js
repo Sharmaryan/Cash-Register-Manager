fc1db09a44dd97917d47d0c6bf980df4b66cdcd8
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2021-09-23 09:29:46 +0530

    fix : empty value bug

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ const billInput = document.querySelector("#bill-amount");
 const cashGivenInput = document.querySelector("#cash-given");
 
 const showMsg = document.querySelector(".show-message");
-const errorMsg = document.querySelector(".error-msg");
 
 const nextBtn = document.querySelector("#next-btn");
 const checkBtn = document.querySelector("#check-btn");
@@ -16,40 +15,49 @@ const notesList = [2000, 500, 100, 20, 10, 5, 1];
 cashGivenSection.style.display = "none";
 table.style.display = "none";
 showMsg.style.display = "none";
-errorMsg.style.display = "none";
 
 nextBtn.addEventListener("click", () => {
-  if (billInput.value > 0) {
+  if (billInput.value >= 1) {
     showMsg.style.display = "none";
     cashGivenSection.style.display = "block";
     nextBtn.style.display = "none";
-  } else {
+  } else if (billInput.value == "0") {
     showMsg.style.display = "block";
     showMsg.innerText = "Please Enter Valid Bill Amount";
+  } else {
+    showMsg.style.display = "block";
+    showMsg.innerText = "you can't leave bill amount empty";
   }
 });
 
 function clickHandler() {
   table.style.display = "block";
-  errorMsg.style.display = "none";
+  showMsg.style.display = "none";
 
   const billInputValue = Number(billInput.value);
   const cashGivenInputValue = Number(cashGivenInput.value);
 
-  if (billInput.value < 0) {
-    errorMsg.style.display = "block";
-    errorMsg.innerText = "Please Enter Valid Bill Amount";
-    table.style.display = "none";
-  } else if (cashGivenInput.value < 0) {
-    errorMsg.style.display = "block";
-    errorMsg.innerText = "Please Enter Valid Cash Given Amount";
-    table.style.display = "none";
+  if (
+    cashGivenInput.value == "" ||
+    billInput.value == "" ||
+    cashGivenInput.value < 0 ||
+    billInput.value < 0
+  ) {
+    if (billInput.value < 0 || cashGivenInput.value < 0) {
+      showMsg.style.display = "block";
+      showMsg.innerText = "Please Enter Valid Amount";
+      table.style.display = "none";
+    } else {
+      showMsg.style.display = "block";
+      showMsg.innerText = " don't leave any field empty";
+      table.style.display = "none";
+    }
   } else {
     if (billInputValue <= cashGivenInputValue) {
       if (billInputValue == cashGivenInputValue) {
         table.style.display = "none";
-        errorMsg.style.display = "block";
-        errorMsg.innerText = `No need for change`;
+        showMsg.style.display = "block";
+        showMsg.innerText = `No need for change`;
       }
 
       let returnedCash = cashGivenInputValue - billInputValue;
@@ -59,8 +67,8 @@ function clickHandler() {
         numberOfNotes[i].innerText = noOfNotes;
       }
     } else {
-      errorMsg.style.display = "block";
-      errorMsg.innerText = "do you wanna wash plates?";
+      showMsg.style.display = "block";
+      showMsg.innerText = "do you wanna wash plates?";
       table.style.display = "none";
     }
   }
